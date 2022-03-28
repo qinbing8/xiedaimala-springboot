b3816c1688c37cdd15db97d1c08849d178cb8aab
BingQin <qinbing08@gmail.com>
2022-03-28 13:20:01 +0800

    Done BlogServiceTest

--- src/test/java/hello/service/BlogServiceTest.java
+++ src/test/java/hello/service/BlogServiceTest.java
@@ -1,19 +1,22 @@
 package hello.service;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyInt;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import hello.dao.BlogDao;
+import hello.entity.Blog;
 import hello.entity.BlogResult;
 import hello.entity.Result;
+import hello.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BlogServiceTest {
@@ -56,4 +59,33 @@ public class BlogServiceTest {
         Assertions.assertEquals("系统异常", blog.getMsg());
     }
 
+    @Test
+    public void returnFailureWhenBlogNotFount() {
+        when(blogDao.selectBlogById(1)).thenReturn(null);
+
+        BlogResult result = blogService.deleteBlog(1, mock(User.class));
+
+        Assertions.assertEquals("fail", result.getStatus());
+        Assertions.assertEquals("博客不存在", result.getMsg());
+    }
+
+    @Test
+    public void returnFailureWhenBlogUserIdNotMatch() {
+        User blogAuthor = new User(123, "blogAuthor", "");
+        User operator = new User(456, "operator", "");
+
+        Blog targetDB = new Blog();
+        targetDB.setId(1);
+        targetDB.setUser(operator);
+
+        Blog blogDB = new Blog();
+        blogDB.setUser(blogAuthor);
+
+        when(blogDao.selectBlogById(1)).thenReturn(blogDB);
+
+        BlogResult blogResult = blogService.updateBlog(1, targetDB);
+
+        Assertions.assertEquals("fail",blogResult.getStatus());
+        Assertions.assertEquals("无法修改别人的博客",blogResult.getMsg());
+    }
 }
